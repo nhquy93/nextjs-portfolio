e9d5a2dd0575b134a083909cebef2403ab1b5c8e
nhquy93 <nhquy93@gmail.com>
2023-07-25 11:03:11 +0700

    nextjs portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css';
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <div>
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+import ProjectsBtn from "../components/ProjectsBtn";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div
+          className="text-center flex flex-col justify-center
+        xl:pt-40 xl:text-left h-full container mx-auto"
+        >
+          <motion.h1
+            variants={fadeIn("down", 0, 3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("down", 0, 3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </motion.p>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div
+          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right 
+        xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
